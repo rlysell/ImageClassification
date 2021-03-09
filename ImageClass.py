1c743c389a9c3465a3638f163f8f8622021e4fd1
rlysell <rasmusen186@gmail.com>
2021-03-09 19:53:32 +0100

    Added right epochs, lr and plotting

--- ImageClass.py
+++ ImageClass.py
@@ -8,7 +8,7 @@ from tensorflow.python.keras.layers.pooling import MaxPool2D
 
 data = keras.datasets.cifar10
 
-EPOCHS = 2
+EPOCHS = 100
 BATCH_SIZE = 32
 val_split = 0.2
 
@@ -22,24 +22,28 @@ train_images = train_images.reshape(train_images.shape[0], 32, 32, 3)
 test_images = test_images.reshape(test_images.shape[0], 32, 32, 3)
 
 loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-optimizer = keras.optimizers.Adam(lr=0.05)
-callback = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+optimizer = keras.optimizers.Adam(lr=0.001)
+callback = keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=3, restore_best_weights=True)
 
 def def_model():
     model = keras.Sequential([
         Conv2D(64, (3, 3), input_shape=(
             32, 32, 3), activation='relu'),
+        Conv2D(64, (3, 3), input_shape=(
+            32, 32, 3), activation='relu'),
         MaxPool2D((2, 2)),
+        Conv2D(128, (3, 3), activation='relu'),
         Conv2D(128, (3, 3), activation='relu'),
         MaxPool2D((2, 2)),
         Conv2D(256, (3, 3), activation='relu'),
+        Conv2D(256, (3, 3), activation='relu'),
         MaxPool2D((2, 2)),
         Flatten(input_shape=()),
-        Dense(1024, activation='relu'),
-        Dense(512, activation='relu'),
-        Dropout(0.2),
-        Dense(256, activation='relu'),
+        #Dense(1024, activation='relu', kernel_regularizer=keras.regularizers.L2(0.05)),
+        #Dense(512, activation='relu', kernel_regularizer=keras.regularizers.L1(0.05)),
+        #Dense(256, activation='relu'),
         Dense(128, activation='relu'),
+        Dropout(0.5),
         Dense(64, activation='relu'),
         Dense(10, activation='softmax')
     ])
@@ -51,18 +55,32 @@ def train_model(model):
     return history, model
 
 def load_model():
-    tf.saved_model.load('image_classification.h5')
+    model = tf.keras.models.load_model('saved_image_model')
+    return model
 
 def plot_model(history):
     plt.figure(figsize=(32, 32))
     plt.plot(history.history['accuracy'])
 
-model = def_model()
+
 try:
-    history, model = train_model(model)
+    model = load_model()
 except:
-    print("An error occured")
-try:
-    tf.saved_model.save(model, ' ')
-except:
-    print('Unable to write to file')
+    model = def_model()
+    try:
+        history, model = train_model(model)
+    except:
+        print("An error occured")
+    try:
+        model.save('saved_image_model')
+    except:
+        print('Unable to write to file')
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=1)
+
+predictions = model.predict(test_images)
+for i in range(5):
+    plt.figure()
+    plt.imshow(test_images[i])
+    plt.title("Predicted " + classes[np.argmax(predictions[i])] + ", should have been " + classes[test_labels[i][0]])
+    plt.xlabel(f"{round(np.max(predictions[i])*100)} % certain")
